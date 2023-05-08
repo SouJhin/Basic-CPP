6d08545d48d3b95316149e76b05f2d615451ac6b
SouJhin <soujhin2022@gmail.com>
2023-05-09 05:52:39 +0800

    5.9

--- src/Easyx/1_13.cpp
+++ src/Easyx/1_13.cpp
@@ -1,18 +1,14 @@
 /*
  * @Author: SouJhin soujhin2022@gmail.com
- * @Date: 2023-01-11 22:40:22
+ * @Date: 2023-01-13 07:03:54
  * @LastEditors: SouJhin soujhin2022@gmail.com
- * @LastEditTime: 2023-01-13 07:03:05
- * @FilePath: \Basic-CPP\src\1.11\main.cpp
+ * @LastEditTime: 2023-01-13 07:04:47
+ * @FilePath: \Basic-CPP\src\Easyx\1_13.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include<iostream>
-using namespace std;
+
 int main() {
-    int a, b, result;
-    cout<<"input two number";
-    cin>>a>>b;
-    cout<<"result is"<<a+b;
-    cin>>a;
+    cout << "fuck u" << endl;
     return 0;
 }
